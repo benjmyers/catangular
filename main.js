36c9fa73a0264f0ad22c009e5fc21a382940f7b6
meredithmmyers <myers.mm89@gmail.com>
2014-02-13 23:16:40 -0500

    Update CSS to properly position the cats

--- main.js
+++ main.js
@@ -4,6 +4,10 @@
 
 var app = angular.module("app",[]);
 
+app.controller("AppCtrl", function ($scope) {
+    $scope.clickCt = 0;
+})
+
 app.directive("cats", function() {
    return {
        restrict: "E",
@@ -13,9 +17,8 @@ app.directive("cats", function() {
        templateUrl: "allCats.html",
        link: function(scope, elem, attrs) {
         scope.meOW = function(ev) {
-
             var images = ['cat1.jpg','cat2.png','cat3.png'];
-            elem.append("<img src='img/"+images[0]+"' style='position:relative; top:"+ev.y+"; left:"+ev.x+";'/>");
+            elem.append("<img src='img/"+images[0]+"' style='position:absolute; top:"+ev.y+"px; left:"+ev.x+"px;'/>");
         }
        }
    }
